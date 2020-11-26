0bb766c34c529894bb7d892b0656dcc4201059f6
dremtri_mao@huatek.com <dremtri_mao@huatek.com>
2020-11-26 13:28:45 +0800

    增加环境变量

--- build/webpack.prod.conf.js
+++ build/webpack.prod.conf.js
@@ -1,7 +1,69 @@
-const { plugins } = require("./webpack.base.conf");
-module.exports = {
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { merge } = require('webpack-merge')
+const { CleanWebpackPlugin } = require("clean-webpack-plugin")
+const baseConf = require('./webpack.base.conf')
+const config = require('./config')
+const { resolve } = require('./utils')
+
+module.exports = merge(baseConf, {
   mode: 'production',
   devtool: config.build.productionSourceMap ? config.build.devtool : false,
+  // optimization: {
+  //   splitChunks: {
+  //     chunks: 'async',
+  //     minSize: 30000,
+  //     maxSize: 0,
+  //     minChunks: 1,
+  //     maxAsyncRequests: 5,
+  //     maxInitialRequests: 3,
+  //     automaticNameDelimiter: '~',
+  //     name: true,
+  //     cacheGroups: {
+  //       vendors: {
+  //         test: /[\\/]node_modules[\\/]/,
+  //         priority: -10
+  //       },
+  //       default: {
+  //         minChunks: 2,
+  //         priority: -20,
+  //         reuseExistingChunk: true
+  //       }
+  //     }
+  //   }
+  // }
+  optimization:{
+    splitChunks: {
+      cacheGroups: {
+        commons: {
+          test: /[\\/]node_modules[\\/]/,
+          name: "vendor",
+          chunks: "all",
+          priority: -10
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true
+        }
+      }
+    },
+    // minimizer: [
+    //   new UglifyJsPlugin({
+    //     uglifyOptions: {
+    //       compress: {
+    //         warnings: false,
+    //         drop_debugger: false,
+    //         drop_console: true
+    //       }
+    //     }
+    //   }),
+    //   new OptimizeCSSAssetsPlugin({
+    //     cssProcessorOptions: {
+    //       safe: true
+    //     }
+    //   })
+    // ]
+  },
   plugins: [
     new CleanWebpackPlugin({
       verbose: true, //开启在控制台输出信息
@@ -13,4 +75,4 @@ module.exports = {
       ]
     }),
   ]
-}
+})
