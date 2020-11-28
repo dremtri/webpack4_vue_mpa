c32bc7d565c9f681d9768eef1fc942db9f463a05
dremtri <dremtri@163.com>
2020-11-28 16:11:58 +0800

    'test'

--- src/pages/home/components/test.vue
+++ src/pages/home/components/test.vue
@@ -5,7 +5,17 @@
 </template>
 <script>
 export default {
-  name: 'Test'
+  name: 'Test',
+  data() {
+    return {
+      arr: [1, 2, 3, 4]
+    }
+  },
+  mounted() {
+    this.arr.forEach(val => console.log(val))
+    const tempArr = this.arr.map(val => val * 2)
+    tempArr.forEach(val => console.log(val))
+  }
 }
 </script>
 
