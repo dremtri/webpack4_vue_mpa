891f2c31861de5ba227f0e4a0f1f07305d7eb777
dremtri_mao@huatek.com <dremtri_mao@huatek.com>
2020-12-01 19:45:52 +0800

    注释cdn

--- build/cdn.js
+++ build/cdn.js
@@ -1,17 +1,17 @@
 /**
- * @file 配置cdn
+ * @file 配置cdn, 有 bug
  */
 module.exports = {
   cdn: {
     css: [
-      'https://lib.baomitu.com/element-ui/2.14.1/theme-chalk/index.min.css',
-      'https://lib.baomitu.com/normalize/8.0.1/normalize.min.css'
+      // 'https://lib.baomitu.com/element-ui/2.14.1/theme-chalk/index.min.css',
+      // 'https://lib.baomitu.com/normalize/8.0.1/normalize.min.css'
     ],
     js: [
-      'https://lib.baomitu.com/element-ui/2.14.1/index.min.js'
+      // 'https://lib.baomitu.com/element-ui/2.14.1/index.min.js'
     ]
   },
   externals: {
-    'element-ui': 'ElementUI'
+    // 'element-ui': 'ElementUI'
   }
 }
